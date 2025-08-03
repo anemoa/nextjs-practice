ee79344ed33b35fd802d95717c71735d1a120a02
anemoa <anemoia.yves@gmail.com>
2025-08-03 23:18:16 +0900

    Add navigation links between home and about pages

--- 01-start/app/about/page.js
+++ 01-start/app/about/page.js
@@ -1,6 +1,12 @@
+import Link from "next/link";
+
 const About = () => {
   return (
-	<div>page</div>
+	<div>
+		<h1>About 페이지</h1>
+		<p>여긴 어바웃 페이지다</p>
+		<Link href="/">홈 가기</Link>
+	</div>
   )
 }
 
--- 01-start/app/page.js
+++ 01-start/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+		<nav>
+			<Link href="/about">어바웃 페이지 가기</Link>
+		</nav>
         <Image
           className={styles.logo}
           src="/next.svg"
