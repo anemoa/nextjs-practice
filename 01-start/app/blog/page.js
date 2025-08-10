802ec78972e1d9699654fa44d6fee8f0f7709348
anemoa <anemoia.yves@gmail.com>
2025-08-10 23:19:34 +0900

    feat: implement dynamic blog data with real content

--- 01-start/app/blog/page.js
+++ 01-start/app/blog/page.js
@@ -1,3 +1,4 @@
+import { blogPosts } from "@/data/blogPosts";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,21 +7,19 @@ const Blog = () => {
     <div>
       <h1>blog page!!!</h1>
       <ul>
-        <li>
-          <Link href="/blog/1">1번 글</Link>
-          <Image
-            src="/img01.jpg"
-            alt={`1번 글 이미지`}
-            width={100}
-            height={300}
-          />
-        </li>
-        <li>
-          <Link href="/blog/2">2번 글</Link>
-        </li>
-        <li>
-          <Link href="/blog/3">3번 글</Link>
-        </li>
+        {blogPosts.map(({ id, title, image }) => {
+          return (
+            <li key={id}>
+              <Link href={`/blog/${id}`}>{title}</Link>
+              <Image
+                src={image}
+                alt={`${title} 이미지`}
+                width={100}
+                height={300}
+              />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
---
+++ 01-start/data/blogPosts.js
@@ -0,0 +1,23 @@
+export const blogPosts = [
+  {
+    id: 1,
+    title: "Next.js 첫 걸음",
+    content: "오늘 Next.js를 시작했어요. 파일 기반 라우팅이 정말 편리하네요! React보다 설정할 게 적어서 좋아요.",
+    date: "2025-08-08",
+    image: "/img01.jpg"
+  },
+  {
+    id: 2,
+    title: "컴포넌트 분리하기",
+    content: "Navigation 컴포넌트를 분리해봤어요. 코드가 훨씬 깔끔해졌습니다. 재사용성도 좋아지고요.",
+    date: "2025-08-09",
+    image: "/img01.jpg"
+  },
+  {
+    id: 3,
+    title: "다이나믹 라우팅 배우기",
+    content: "URL 파라미터를 받아서 동적인 페이지를 만들어봤어요. [id] 폴더 만드는 게 신기했어요!",
+    date: "2025-08-10",
+    image: "/img01.jpg"
+  }
+];
