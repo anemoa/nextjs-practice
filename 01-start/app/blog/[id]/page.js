1299145abda5c3c0c635da526cbac4f158b71eb3
anemoa <anemoia.yves@gmail.com>
2025-08-13 23:40:10 +0900

    feat: integrate external API for blog data

--- 01-start/app/blog/[id]/page.js
+++ 01-start/app/blog/[id]/page.js
@@ -1,33 +1,32 @@
-import { blogPosts } from "@/data/blogPosts";
-import Image from "next/image";
 import { notFound } from "next/navigation";
-import React from "react";
 
-const BlogPost = ({ params }) => {
-  const { id } = params;
 
-  // id와 일치하는 글 찾기
-  const post = blogPosts.find(post => post.id === parseInt(id));
+const getBlogPost = async (id) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
 
-  // 글이 없으면 리턴
-  if(!post){
-	notFound();
+  if (!res.ok) {
+    return null; // 글이 없으면 null return
   }
 
-  // 구조분해
-  const {title, content, image, date} = post;
+  return res.json();
+};
+
+const BlogPost = async ({ params }) => {
+  const { id } = params; // API에서 글 가져오기
+
+  const post = await getBlogPost(id);
+  // 글이 없으면 리턴
+  if (!post) {
+    notFound(); // Next.js 가 제공하는 특별 함수 404 페이지로 전환해줌
+  }
 
   return (
     <div>
-      <h1>{title}</h1>
-	  <p>{content}</p>
-      <Image
-        src={image}
-        alt={`${id}번 글 이미지`}
-        width={200}
-        height={400}
-      />
-		<p>{date}</p>
+      <h1>{post.title}</h1>
+      <p>
+        <strong>글 번호:</strong> {post.id}
+      </p>
+      <p>{post.body}</p>
     </div>
   );
 };
--- 01-start/app/blog/page.js
+++ 01-start/app/blog/page.js
@@ -1,22 +1,25 @@
-import { blogPosts } from "@/data/blogPosts";
 import Image from "next/image";
 import Link from "next/link";
 
-const Blog = () => {
+const getBlogPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  // 10개 가져오기
+  return posts.slice(0, 10);
+};
+
+const Blog = async () => {
+  const blogPosts = await getBlogPosts();
+
   return (
     <div>
       <h1>blog page!!!</h1>
       <ul>
-        {blogPosts.map(({ id, title, image }) => {
+        {blogPosts.map(({ id, title }) => {
           return (
-            <li key={id}>
+            <li key={id} style={{ marginBottom: "10px" }}>
               <Link href={`/blog/${id}`}>{title}</Link>
-              <Image
-                src={image}
-                alt={`${title} 이미지`}
-                width={100}
-                height={300}
-              />
             </li>
           );
         })}
