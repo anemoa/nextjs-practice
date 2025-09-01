4e10f718ee8b48b3fe3f47e079a752282c2e30fa
anemoa <anemoia.yves@gmail.com>
2025-09-01 23:55:35 +0900

    feat: add dynamic routing file and single post api

--- 02-next-with-typescript/lib/api.ts
+++ 02-next-with-typescript/lib/api.ts
@@ -10,3 +10,14 @@ export async function getAllPosts(): Promise<Post[]> {
   const posts: Post[] = await response.json();
   return posts;
 }
+
+export async function getPostById(id: string): Promise<Post> {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  
+  if (!response.ok) {
+    throw new Error('글을 가져오는데 실패했습니다');
+  }
+  
+  const post: Post = await response.json();
+  return post;
+}
