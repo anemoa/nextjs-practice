2e8aabc30c6d8ce92887db25cf5f5c7dc8272754
anemoa <anemoia.yves@gmail.com>
2025-09-16 23:40:18 +0900

    feat: add todo list page basic structure

---
+++ 02-next-with-typescript/pages/todo.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+const TodoPage = () => {
+  return (
+	<Layout>
+		<div>
+			<h1>할 일 관리하기</h1>
+		</div>
+	</Layout>
+  )
+}
+
+export default TodoPage;
