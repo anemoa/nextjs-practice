7d60af8a000e4c444e06be4aab179fbb2804ef90
anemoa <anemoia.yves@gmail.com>
2025-10-04 23:47:27 +0900

    feat: add product dummy data

---
+++ 03-cart-shopping/data/products.ts
@@ -0,0 +1,28 @@
+import { Product } from '@/types/product';
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: '무선 이어폰',
+    price: 89000,
+    image: 'https://via.placeholder.com/200'
+  },
+  {
+    id: 2,
+    name: '노트북 파우치',
+    price: 25000,
+    image: 'https://via.placeholder.com/200'
+  },
+  {
+    id: 3,
+    name: '휴대폰 케이스',
+    price: 15000,
+    image: 'https://via.placeholder.com/200'
+  },
+  {
+    id: 4,
+    name: '마우스',
+    price: 35000,
+    image: 'https://via.placeholder.com/200'
+  }
+];
